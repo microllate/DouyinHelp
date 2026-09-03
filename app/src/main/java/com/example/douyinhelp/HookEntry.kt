f0bbb75220fd0b4b06a7ad792ef0a7d876b69533
microllate <120251959+microllate@users.noreply.github.com>
2026-09-03 16:49:07 +0800

    Create HookEntry.kt

---
+++ app/src/main/java/com/example/douyinhelp/HookEntry.kt
@@ -0,0 +1,22 @@
+package com.example.douyinhelp
+
+import com.highcapable.yukihookapi.YukiHookAPI
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.type.android.ContextClass
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+class HookEntry : IYukiHookXposedInit {
+
+    override fun onInit() = configs {
+        isDebug = true
+    }
+
+    override fun onHook() = encase {
+        loadApp(name = "com.ss.android.ugc.aweme") {
+            // Hook 抖音双击手势逻辑预留位置
+        }
+    }
+}
